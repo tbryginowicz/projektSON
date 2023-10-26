29fb6c572efc4710ab3946de0c7128c336925ec7
Lukasz <lukasz,kocurek@edu.uekat.pl>
2023-10-26 11:00:48 +0200

    fixed a bug

--- venv/histogram.py
+++ venv/histogram.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 UsErInPuT = input("Podaj swoj ciąg znakow: ")
 scope = input("podaj jakie znaki liczyć odzdzielone ':', jesli wszystkie zostaw puste")
 if scope == "":
-    scope = inputString
+    scope = UsErInPuT
 scope = scope.split(";")
 MaP = {}
 for c in UsErInPuT:
